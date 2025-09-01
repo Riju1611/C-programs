cf50254a320c0bf82d2ab81352c9731a3d7774f0
Riju Chauhan <riju112006@gmail.com>
2025-09-01 19:05:41 +0530

    Add files via upload

---
+++ Riju_C-program-Q3.1.c
@@ -0,0 +1,11 @@
+//1. Print 1st 10 natural nos.
+#include <stdio.h>
+int main()
+{
+    int i;
+    for(i=1;i<=10;i++)
+    {
+        printf("%d\n",i);
+    }
+    return 0;
+}
---
+++ Riju_C-program-Q3.2.c
@@ -0,0 +1,11 @@
+//2.Print 1st 10 odd nos.
+#include <stdio.h>
+int main()
+{
+    int i;
+    for(i=1;i<=19;i+=2)
+    {
+        printf("%d\n",i);
+    }
+    return 0;
+}
