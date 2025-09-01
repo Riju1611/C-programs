f18687500f5bd67ae77305fafc67bb40f8d8a019
Riju Chauhan <riju112006@gmail.com>
2025-09-01 18:16:05 +0530

    Add files via upload

---
+++ Riju_C-program-Q2.2.c
@@ -0,0 +1,34 @@
+//2. Find out largest and smallest of three values.
+#include <stdio.h>
+int main()
+{
+    int a,b,c;
+    printf("Enter three numbers:");
+    scanf("%d %d %d", &a, &b, &c);
+    if (a>=b && a>=c)
+    {
+        printf("largest=%d",a);
+    }
+    else if (b>=a && b>=c)
+    {
+        printf("largest=%d",b);
+    }
+    else
+    {
+        printf("largest=%d",c);
+    }
+
+    if (a<=b && a<=c)
+    {
+        printf("smallest=%d",a);
+    }
+    else if (b<=a && b<=c)
+    {
+        printf("smallest=%d",b);
+    }
+    else
+    {
+        printf("smallest=%d",c);
+    }
+    return 0;
+}
